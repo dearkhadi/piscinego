fb5e556f1e3cd90d2c96551264bbfd45bdc782a0
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-07 02:26:40 +0500

    2

--- test/main.go
+++ test/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.ReverseMenuIndex([]string{"desserts", "mains", "drinks", "starters"}))
+	fmt.Println(piscine.ReverseMenuIndex([]string{"starters", "mains", "drinks", "desserts"}))
 }
