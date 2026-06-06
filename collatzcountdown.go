98c421b8403a496c608e5016337bb6078d694f3f
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-06 14:33:21 +0500

    collatz

---
+++ collatzcountdown.go
@@ -0,0 +1,21 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	counter := 0
+
+	if start <= 0 {
+		return -1
+	}
+
+	for start > 1 {
+		if start%2 == 0 {
+			start = start / 2
+			counter++
+		} else {
+			start = start*3 + 1
+			counter++
+		}
+	}
+
+	return counter
+}
