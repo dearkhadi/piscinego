a8f331712e0c14c23e1aa2eecf3082ace89aca0f
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-07 02:40:40 +0500

    2

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -2,9 +2,10 @@ package piscine
 
 func ReverseMenuIndex(menu []string) []string {
 	answer := make([]string, len(menu))
+	n := len(menu) // Сохраним длину для удобства
 
-	for i, str := range menu {
-		answer[len(answer)-1-i] = str
+	for i := 0; i < n; i++ {
+		answer[n-1-i] = menu[i]
 	}
 
 	return answer
