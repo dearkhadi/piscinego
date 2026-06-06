9401575279afa4fe226e43e10afcc1c6761b8562
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-07 01:24:39 +0500

    3

---
+++ comcheck/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	myArg := os.Args[1:]
+
+	for _, arg := range myArg {
+		switch arg {
+		case "01":
+			fmt.Println("Alert!!!")
+			return
+		case "galaxy":
+			fmt.Println("Alert!!!")
+			return
+		case "galaxy 01":
+			fmt.Println("Alert!!!")
+			return
+		}
+	}
+	return
+}
