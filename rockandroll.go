99e6c3f5bde68bc31349bbca371fe9b75476cebf
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-07 08:22:00 +0500

    2

--- rockandroll.go
+++ rockandroll.go
@@ -4,17 +4,17 @@ func RockAndRoll(n int) string {
 	answer := ""
 
 	if n < 0 {
-		answer = "error: number is negative"
+		answer = "error: number is negative\n"
 	} else if n%2 != 0 && n%3 != 0 {
-		answer = "error: number is negative"
+		answer = "error: number is negative\n"
 	}
 
 	if n%2 == 0 && n%3 == 0 {
-		answer = "rock and roll"
+		answer = "rock and roll\n"
 	} else if n%2 == 0 {
-		answer = "rock"
+		answer = "rock\n"
 	} else if n%3 == 0 {
-		answer = "roll"
+		answer = "roll\n"
 	}
 
 	return answer
