68ce49f4f7737bbbcea4882ed237201d31185615
khaaleeva <aleeva.khadi4a@gmail.com>
2026-06-07 08:28:46 +0500

    2

--- rockandroll.go
+++ rockandroll.go
@@ -1,21 +1,17 @@
 package piscine
 
 func RockAndRoll(n int) string {
-	answer := ""
-
 	if n < 0 {
-		answer = "error: number is negative\n"
-	} else if n%2 != 0 && n%3 != 0 {
-		answer = "error: number is negative\n"
+		return "error: number is negative\n"
 	}
 
 	if n%2 == 0 && n%3 == 0 {
-		answer = "rock and roll\n"
+		return "rock and roll\n"
 	} else if n%2 == 0 {
-		answer = "rock\n"
+		return "rock\n"
 	} else if n%3 == 0 {
-		answer = "roll\n"
+		return "roll\n"
 	}
 
-	return answer
+	return "error: non divisible\n"
 }
