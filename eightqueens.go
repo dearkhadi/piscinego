c53f9cbf90a53ec4a84fafd67a8fd40c091fc82a
dearkhadi <aleeva.khadi4a@gmail.com>
2026-06-09 17:58:43 +0500

    list

--- eightqueens.go
+++ eightqueens.go
@@ -4,24 +4,28 @@ import "github.com/01-edu/z01"
 
 func EightQueens() {
 	var board [8]int
+	size := len("........")
+	one := len("a")
+	zero := len("")
+
 	var solve func(int)
 	solve = func(col int) {
-		if col == 8 {
-			for i := 0; i < 8; i++ {
+		if col == size {
+			for i := zero; i < size; i++ {
 				z01.PrintRune(rune(board[i] + '0'))
 			}
 			z01.PrintRune('\n')
 			return
 		}
-		for row := 1; row <= 8; row++ {
+		for row := one; row <= size; row++ {
 			safe := true
-			for i := 0; i < col; i++ {
+			for i := zero; i < col; i++ {
 				rowDiff := board[i] - row
-				if rowDiff < 0 {
+				if rowDiff < zero {
 					rowDiff = -rowDiff
 				}
 				colDiff := i - col
-				if colDiff < 0 {
+				if colDiff < zero {
 					colDiff = -colDiff
 				}
 				if board[i] == row || rowDiff == colDiff {
@@ -31,9 +35,9 @@ func EightQueens() {
 			}
 			if safe {
 				board[col] = row
-				solve(col + 1)
+				solve(col + one)
 			}
 		}
 	}
-	solve(0)
+	solve(zero)
 }
