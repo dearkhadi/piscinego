913fa2e7f63e830b6c77ab806904cfbd7390f906
dearkhadi <aleeva.khadi4a@gmail.com>
2026-06-10 22:20:01 +0500

    listfind

--- listfind.go
+++ listfind.go
@@ -1,5 +1,9 @@
 package piscine
 
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
 	if l == nil {
 		return nil
