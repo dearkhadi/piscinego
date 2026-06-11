f2a0d07ed059f8bafbe5af8cbb54fd244afeaaff
dearkhadi <aleeva.khadi4a@gmail.com>
2026-06-11 17:18:58 +0500

    btreeapply2

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -7,6 +7,7 @@ func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 
 	f(root.Data)
 
-	BTreeApplyPostorder(root.Left, f)
-	BTreeApplyPostorder(root.Right, f)
+	BTreeApplyPreorder(root.Left, f)
+
+	BTreeApplyPreorder(root.Right, f)
 }
